6d67c16563a25bf1834f8c4ec35e9f76c5ee060c
Veerrohit Veeravadivel <veerrohitv08@gmail.com>
2022-11-06 09:54:39 -0800

    testing out app dir

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    appDir: "true",
+    transpilePackages: ["ui"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
